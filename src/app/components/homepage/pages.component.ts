05f8faffe03990a5bb28fd4214bed819128d5444
vickler charles <charlesvikler@gmail.com>
2019-07-19 13:01:45 -0500

    mas edicones de logo

--- src/app/components/homepage/pages.component.ts
+++ src/app/components/homepage/pages.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-apges',
   templateUrl: './pages.component.html',
 })
+
 export class PagesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(public router: Router) { }
+  ngOnInit() {}
 
 }
+
